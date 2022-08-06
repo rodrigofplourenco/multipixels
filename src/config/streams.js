e32ded5ec2240f09a9d758f2ac489fa8a50316bd
Rodrigo Lourenço <rodrigofplourenco2002@gmail.com>
2022-08-06 19:59:27 +0100

    [REM] Removed RadioHabblet stream

--- src/config/streams.js
+++ src/config/streams.js
@@ -1,13 +1,5 @@
 const streams = [
   {
-    "thumbnail": "/img/playerRadioHabblet.jpg",
-    "title": "RadioHabblet",
-    "genre": "Diverso",
-    "data-artist": "Ao Vivo",
-    "url": "https://stream2.svrdedicado.org/8094/stream"
-  },
-
-  {
     "thumbnail": "https://avatar.blet.in/Mekinino&action=std&size=b&head_direction=3&direction=4&gesture=sml&headonly=0",
     "title": "Multipixels",
     "genre": "Diverso",
